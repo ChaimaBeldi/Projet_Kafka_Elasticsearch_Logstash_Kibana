504c3b4d7c9f1fd459384461789c27cc40993360
Chaima <64742651+ChaimaBeldi@users.noreply.github.com>
2022-01-08 21:23:04 +0100

    Update covid-consumer.py

--- covid-consumer.py
+++ covid-consumer.py
@@ -1,43 +1,32 @@
 import json
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer("covid-19", bootstrap_servers='localhost:9092')
+###
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+####
+from datetime import datetime
+
+
+consumer = KafkaConsumer("corona-19", bootstrap_servers='localhost:9092')
 
 for message in consumer:
     print(message)
-    stat = message.value.decode()
-    #print(stat)    
-    # stat_resp = stat["response"]
- #import sys
- #import json
- #from pyspark.sql import SparkSession
- #from pyspark import SparkContext
- #from pyspark.sql import SQLContext
- #from pyspark.sql.functions import *
- #from pyspark.sql.types import *
- #import findspark
- #findspark.init("/opt/spark")
- #import pyspark
-
- #conf = pyspark.SparkConf()
-#conf.set('spark.app.name', DataStreamingApp) # Optional configurations
-
-# init & return
- #sc = pyspark.SparkContext.getOrCreate(conf=conf)
- #sqlcontext = SQLContext(sc)
- #spark = SparkSession.builder.appName('DataStreamingApp').getOrCreate()
-
- #df = spark \
-   #.readStream \
-   #.format("kafka") \
-   #.option("kafka.bootstrap.servers", "localhost:9092") \
-   #.option("subscribe", "Data_Streaming") \
-   #.load()
- #query = df \
-   #  .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
-     #.writeStream \
-     #.format("console") \
-     #.start()
- #query.awaitTermination() 
-
+    #stat = message.value.decode() 
+    country= stat["country"]  
+    lastUpdatedDate= stat["lastUpdatedDate"]
     
+    population= stat["population"]
+    metrics= stat["metrics"]
+    actuals= stat["actuals"]
+    cases = actuals["cases"]
+    deaths = actuals["deaths"]
+    positiveTests = actuals["positiveTests"]
+    negativeTests = actuals["negativeTests"]
+    hospitalBeds = actuals["hospitalBeds"] 
+    newCases= actuals["newCases"]
+    newDeaths = actuals["newDeaths"]
+    vaccinesDistributed= actuals["vaccinesDistributed"]
+    vaccinesInitiated= actuals["vaccinationsCompleted"]
+    vaccinesCompleted= actuals["vaccinationsCompleted"]
+  
